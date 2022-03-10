9e1821552d31bbe1b1ab521455f44f032a09d25f
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-03-10 18:22:09 +0900

    失敗時の処理 一覧取得

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express') // expressを呼び出す
 const app = express() // expressを実行する
 const port = 3000 // 通信宛先を区別するための仕組み
+const booklog = {} // 使いまわしできるように空で定義
 
 app.use(express.json()) // jsonを使えるように指定する
 
 // 読書ログを記録する=/booklogにPOSTされる
 app.post('/booklog', (req, res) => {
 	// 保存したデータを受け取る処理
-	const booklog = req.body
-	res.json({
+	const booklog = req.body // bodyに値をつける(req.body)
+	
+	// booklogのnameとtextの値がない（空）の場合
+	if(!(booklog.name && booklog.text)) {
+		return res.json({
+			"ok": false,
+			"error": "invalid parameter" // エラー内容
+		})
+	}
+
+	// 成功処理
+	res.json({ // resとしてjsonを返すよ
+		// どんなjsonをかえすか
 		"ok": true,
-		"booklog": booklog
+		"booklog": [
+			booklog
+		]
+	})
+})
+
+// 読書ログの一覧を取得する処理の関数=/booklogにGETしたい
+app.get('/booklog', (req, res) =>{
+	// resとしてjsonを返すよ
+	res.json({
+		// どんなjsonをかえすか
+		"ok": false,
+		"booklog": []
 	})
 })
 
